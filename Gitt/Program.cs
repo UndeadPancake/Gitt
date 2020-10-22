f8a2c213d58290a31e5e3386654bd55c40da6e26
Martin Johansson <maxamartin1999@gmail.com>
2020-10-22 15:40:44 +0200

    Exchanged sqrt for power of two

--- Gitt/Program.cs
+++ Gitt/Program.cs
@@ -9,9 +9,8 @@ namespace Gitt
             //Console.WriteLine("Hello World!");
             for (int i = 1; i <= 10; i++)
             {
-                Console.WriteLine("sqrt({0}) = {1}", i, Math.Sqrt(i));
+                Console.WriteLine("2^{0} = {1}", i, Math.Pow(2, i));
             }
-            Console.ReadKey();
         }
     }
 }
